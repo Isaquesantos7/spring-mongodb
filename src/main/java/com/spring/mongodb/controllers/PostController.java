edbddb8d1f03a387454d1b34ec8aec2f78138060
isaquesantos7 <isaquesantos.second.1998@gmail.com>
2024-06-24 12:59:56 -0300

    feat: Multi-criteria queries

--- src/main/java/com/spring/mongodb/controllers/PostController.java
+++ src/main/java/com/spring/mongodb/controllers/PostController.java
@@ -7,10 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -35,4 +34,15 @@ public class PostController {
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
+    @GetMapping("/api/posts/fullSearch")
+    public ResponseEntity<List<Post>> fullSearch(
+            @RequestParam(value="text", defaultValue="") String text,
+            @RequestParam(value="minDate", defaultValue="") String minDate,
+            @RequestParam(value="maxDate", defaultValue="") String maxDate) {
+        text = URL.decodeParams(text);
+        Date min = URL.convertDate(minDate, new Date(0L));
+        Date max = URL.convertDate(maxDate, new Date());
+        List<Post> list = this.postService.fullSearch(text, min, max);
+        return ResponseEntity.ok().body(list);
+    }
 }
